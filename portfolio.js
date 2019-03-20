08dde002f3ff294c9b0eac84901cfa397c104570
Emily Sanchez <emilysanchez@Emilys-MacBook-Pro.local>
2019-03-20 12:02:55 -0700

    Basic start- just HTML and CSS

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.carousel').carousel();
+    var arr = [1, 2, 3, 4];
+    console.log(arr);
+}); 
